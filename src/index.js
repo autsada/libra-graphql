cd6a584e59d92f38dbc3b5d811c952234e8cd5b6
autsada <autsada.t@gmail.com>
2019-11-02 14:10:54 +0700

    Removed unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 require('dotenv').config({ path: 'config/dev.env' })
 const server = require('./server')
 
-server.start({ port: process.env.PORT }, deets => {
-  console.log(`Server is now running on port http://localhost:${deets.port}`)
-})
+console.log(process.env.NODE_ENV)
+
+server.start(
+  {
+    port: process.env.PORT
+    // subscriptions: {
+    //   path: 'wss://libra-graphql.herokuapp.com/'
+    // }
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`)
+  }
+)
