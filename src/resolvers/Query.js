e7e6ddb9826adecf42a454eefe0e224e1986f1f1
autsada <autsada.t@gmail.com>
2019-11-28 13:10:20 +0700

    Covered error cases for all queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -32,7 +32,9 @@ const Query = {
 
       return accountState
     } catch (error) {
-      throw new Error(error)
+      throw new Error(
+        "Account does not exist, please check your arguments or try again later."
+      )
     }
   },
 
@@ -43,113 +45,125 @@ const Query = {
     { libra },
     info
   ) => {
-    // Check if all arguments provided
-    if (!address || sequenceNumber === null || sequenceNumber === undefined) {
-      throw new Error(`Please provide all required arguments.`)
-    }
-
-    if (typeof address !== "string" || address.length !== 64) {
-      throw new Error(`Please provide a valid account address.`)
-    }
-
-    if (typeof sequenceNumber !== "number") {
-      throw new Error(`Please provide a valid sequence number.`)
-    }
-
-    const res = await libra.queryAccountBySequenceNumber({
-      accountAddress: address,
-      sequenceNumber: sequenceNumber
-    })
-
-    if (
-      !res.response_items[0].get_account_transaction_by_sequence_number_response
-        .transaction_with_proof
-    ) {
-      throw new Error(`Transaction not found.`)
-    }
-
-    const ledger = decodeLedger(res)
-    const accountState = new AccountState(ledger)
-
-    const {
-      transaction_with_proof
-    } = accountState.response_items[0].get_account_transaction_by_sequence_number_response
-
-    if (!transaction_with_proof) {
-      throw new Error(`Transacton not found.`)
-    }
-
-    const {
-      transaction: {
-        transaction: { from_account, to_account }
-      },
-      events: { events }
-    } = transaction_with_proof
-
-    events.map(event => {
-      const { event_data } = event
-
-      if (from_account === event_data.address) {
-        event_data.event_type = "sent"
+    try {
+      // Check if all arguments provided
+      if (!address || sequenceNumber === null || sequenceNumber === undefined) {
+        throw new Error(`Please provide all required arguments.`)
       }
 
-      if (to_account === event_data.address) {
-        event_data.event_type = "received"
+      if (typeof address !== "string" || address.length !== 64) {
+        throw new Error(`Please provide a valid account address.`)
       }
-    })
 
-    return transaction_with_proof
+      if (typeof sequenceNumber !== "number") {
+        throw new Error(`Please provide a valid sequence number.`)
+      }
+
+      const res = await libra.queryAccountBySequenceNumber({
+        accountAddress: address,
+        sequenceNumber: sequenceNumber
+      })
+
+      if (
+        !res.response_items[0]
+          .get_account_transaction_by_sequence_number_response
+          .transaction_with_proof
+      ) {
+        throw new Error(`Transaction not found.`)
+      }
+      const ledger = decodeLedger(res)
+      const accountState = new AccountState(ledger)
+
+      const {
+        transaction_with_proof
+      } = accountState.response_items[0].get_account_transaction_by_sequence_number_response
+
+      if (!transaction_with_proof) {
+        throw new Error(`Transacton not found.`)
+      }
+
+      const {
+        transaction: {
+          transaction: { from_account, to_account }
+        },
+        events: { events }
+      } = transaction_with_proof
+
+      events.map(event => {
+        const { event_data } = event
+
+        if (from_account === event_data.address) {
+          event_data.event_type = "sent"
+        }
+
+        if (to_account === event_data.address) {
+          event_data.event_type = "received"
+        }
+      })
+
+      return transaction_with_proof
+    } catch (error) {
+      throw new Error(
+        `Something went wrong, please check your arguments or try again later.`
+      )
+    }
   },
 
   // Return sent events for specified account address
   querySentEvents: async (parent, { address }, { libra }, info) => {
-    // Check if user provide address for query
-    if (!address) {
-      throw new Error(`Please provide account address.`)
-    }
-
-    // Check if provided adress is in correct format
-    if (typeof address !== "string" || address.length !== 64) {
-      throw new Error(`Please provide a valide account address.`)
-    }
-
-    const res = await libra.queryEventsByAccessPath({
-      accessPath: {
-        address: address,
-        path: accessPath,
-        eventType: "/sent_events_count/"
+    try {
+      // Check if user provide address for query
+      if (!address) {
+        throw new Error(`Please provide account address.`)
       }
-    })
 
-    const ledger = decodeLedger(res)
-    const accountState = new AccountState(ledger)
-
-    const {
-      events_with_proof
-    } = accountState.response_items[0].get_events_by_event_access_path_response
-
-    events_with_proof.map(event => {
-      event.event.event_data.event_type = "sent"
-    })
-
-    const txnVersions = events_with_proof.map(
-      event => event.transaction_version
-    )
-
-    for (let i = 0; i < txnVersions.length; i++) {
-      const res = await libra.queryOneTransaction(txnVersions[i])
-
-      if (res && res.response_items[0].get_transactions_response) {
-        const txnBytes =
-          res.response_items[0].get_transactions_response.txn_list_with_proof
-            .transactions[0].transaction
-
-        const transaction = decodeSignedTxn(txnBytes)
-        events_with_proof[i].expiration_time = transaction.expiration_time
+      // Check if provided adress is in correct format
+      if (typeof address !== "string" || address.length !== 64) {
+        throw new Error(`Please provide a valide account address.`)
       }
-    }
 
-    return events_with_proof
+      const res = await libra.queryEventsByAccessPath({
+        accessPath: {
+          address: address,
+          path: accessPath,
+          eventType: "/sent_events_count/"
+        }
+      })
+
+      const ledger = decodeLedger(res)
+      const accountState = new AccountState(ledger)
+
+      const {
+        events_with_proof
+      } = accountState.response_items[0].get_events_by_event_access_path_response
+
+      events_with_proof.map(event => {
+        event.event.event_data.event_type = "sent"
+      })
+
+      const txnVersions = events_with_proof.map(
+        event => event.transaction_version
+      )
+
+      for (let i = 0; i < txnVersions.length; i++) {
+        const res = await libra.queryOneTransaction(txnVersions[i])
+
+        if (res && res.response_items[0].get_transactions_response) {
+          const txnBytes =
+            res.response_items[0].get_transactions_response.txn_list_with_proof
+              .transactions[0].transaction
+
+          const transaction = decodeSignedTxn(txnBytes)
+          events_with_proof[i].expiration_time = transaction.expiration_time
+        }
+      }
+
+      return events_with_proof
+    } catch (error) {
+      throw new Error(
+        "Something went wrong, please check your arguments or try again later."
+      )
+    }
   },
 
   // Return received events for specified account address
@@ -164,43 +178,49 @@ const Query = {
       throw new Error(`Please provide a valide account address.`)
     }
 
-    const res = await libra.queryEventsByAccessPath({
-      accessPath: {
-        address,
-        path: accessPath,
-        eventType: "/received_events_count/"
+    try {
+      const res = await libra.queryEventsByAccessPath({
+        accessPath: {
+          address,
+          path: accessPath,
+          eventType: "/received_events_count/"
+        }
+      })
+
+      const ledger = decodeLedger(res)
+      const accountState = new AccountState(ledger)
+
+      const {
+        events_with_proof
+      } = accountState.response_items[0].get_events_by_event_access_path_response
+
+      events_with_proof.map(event => {
+        event.event.event_data.event_type = "received"
+      })
+
+      const txnVersions = events_with_proof.map(
+        event => event.transaction_version
+      )
+
+      for (let i = 0; i < txnVersions.length; i++) {
+        const res = await libra.queryOneTransaction(txnVersions[i])
+
+        if (res && res.response_items[0].get_transactions_response) {
+          const txnBytes =
+            res.response_items[0].get_transactions_response.txn_list_with_proof
+              .transactions[0].transaction
+
+          const transaction = decodeSignedTxn(txnBytes)
+          events_with_proof[i].expiration_time = transaction.expiration_time
+        }
       }
-    })
 
-    const ledger = decodeLedger(res)
-    const accountState = new AccountState(ledger)
-
-    const {
-      events_with_proof
-    } = accountState.response_items[0].get_events_by_event_access_path_response
-
-    events_with_proof.map(event => {
-      event.event.event_data.event_type = "received"
-    })
-
-    const txnVersions = events_with_proof.map(
-      event => event.transaction_version
-    )
-
-    for (let i = 0; i < txnVersions.length; i++) {
-      const res = await libra.queryOneTransaction(txnVersions[i])
-
-      if (res && res.response_items[0].get_transactions_response) {
-        const txnBytes =
-          res.response_items[0].get_transactions_response.txn_list_with_proof
-            .transactions[0].transaction
-
-        const transaction = decodeSignedTxn(txnBytes)
-        events_with_proof[i].expiration_time = transaction.expiration_time
-      }
+      return events_with_proof
+    } catch (error) {
+      throw new Error(
+        "Something went wrong, please check your arguments or try again later."
+      )
     }
-
-    return events_with_proof
   },
 
   // querySingleTransaction: async (parent, { version }, { libra }, info) => {
