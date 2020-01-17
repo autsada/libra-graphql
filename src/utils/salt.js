0a43fa4727f1f4e0a0bce9c1344c80c54a7dcccb
autsada <autsada.t@gmail.com>
2020-01-17 13:56:49 +0700

    Updated as latest testnet update

--- src/utils/salt.js
+++ src/utils/salt.js
@@ -3,7 +3,7 @@
 //   salt: "46f174df6ca8de5ad29745f91584bb913e7df8dd162e3e921a5c1d8637c88d16"
 // }
 
-"transaction@@$$LIBRA$$@@"
+// sha3_256.update("RawTransaction::libra_types::transaction@@$$LIBRA$$@@").hex()
 module.exports = {
   salt: "ff08c0e981f4b6acb63446b31e0c5a9393dba612e85b3056247d92f3e33bd834"
 }
